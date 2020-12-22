ef9464700491cbc7060e73a3a0c0148e46758331
Yulia <sivonenkoyulia@gmail.com>
2020-12-22 03:51:32 +0200

    component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+<header className="header">
+        <img src="https://img2.pngio.com/logo-png-images-download-150000-logo-png-resources-with-logo-download-png-360_360.png" />
+      </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+<div className="content">
+        <div>
+          <img
+            src="https://miro.medium.com/max/7680/1*73dOmpiKFUXmWIdFtan4VA.png"
+            alt=""
+          />
+        </div>
+        <div>ava + discription</div>
+        <div>
+          My posts
+          <div>New post</div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile
