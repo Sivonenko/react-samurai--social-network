d4640495feeaca56195c10232ec5aecd49fd1eb8
Yulia <sivonenkoyulia@gmail.com>
2021-01-02 19:12:41 +0200

    route

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogs__items}>
-        <div className={s.items + ' ' + s.active}>Kiwi</div>
-        <div className={s.items}>Polly</div>
-        <div className={s.items}>Molly</div>
-        <div className={s.items}>Mark</div>
-        <div className={s.items}>Andry</div>
-        <div className={s.items}>Ann</div>
+              <div className={s.items + ' ' + s.active}>
+                 <NavLink to='/dialogs/1'>Kiwi</NavLink>
+                  </div>
+        <div className={s.items}><NavLink to='/dialogs/2'>Polly</NavLink></div>
+        <div className={s.items}><NavLink to='/dialogs/3'>Molly</NavLink></div>
+        <div className={s.items}><NavLink to='/dialogs/4'>Mark</NavLink></div>
+        <div className={s.items}><NavLink to='/dialogs/5'>Andry</NavLink></div>
+        <div className={s.items}><NavLink to='/dialogs/6'>Ann</NavLink></div>
       </div>
       <div className={s.massage}>
         <div className={s.massage_items}>Hi</div>
