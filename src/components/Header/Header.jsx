e9f9fe77058b8d9c2346d0376022924a890896aa
Yulia <sivonenkoyulia@gmail.com>
2021-01-05 20:34:10 +0200

    css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import s from './Header.module.css'
 const Header = () => {
     return (
 <header className={s.header}>
-        <img className={s.header__img} src="https://img2.pngio.com/logo-png-images-download-150000-logo-png-resources-with-logo-download-png-360_360.png" />
+        <h2 className={s.headerTitle}>JS</h2>
       </header>
     )
 }
