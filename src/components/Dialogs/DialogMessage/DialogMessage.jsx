95e407bed38613ca13c50d8e80880e00bdc708cb
Yulia <sivonenkoyulia@gmail.com>
2021-01-18 23:54:37 +0200

    l31 message

--- src/components/Dialogs/DialogMessage/DialogMessage.jsx
+++ src/components/Dialogs/DialogMessage/DialogMessage.jsx
@@ -3,7 +3,19 @@ import s from "./../Dialogs.module.css";
 
 
 const Message = (props) => {
-  return <div className={s.message_items}>{props.message}</div>;
+  let newMessageEl = React.createRef();
+let addMessage = () => {
+  let text = newMessageEl.current.value;
+  alert(text)
+}
+  return (
+  <div className={s.message_items}>
+    <textarea className={s.textarea} ref={newMessageEl}></textarea>
+    {props.message}
+    <div>
+          <button onClick={addMessage}>Add post</button>
+          </div></div>
+          )
 };
 
 
