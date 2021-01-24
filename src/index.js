f2e389e2065865e369512b1e6e96f494a6f349ff
Yulia <sivonenkoyulia@gmail.com>
2021-01-24 23:19:19 +0200

    l32

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import state from "./redux/state";
+import { addPost } from './redux/state';
+// addPost('Hello');
 
 ReactDOM.render(
   <React.StrictMode>
-    <App state={state}/>
+    <App state={state} addPost={addPost}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
